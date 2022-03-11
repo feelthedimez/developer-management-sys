2d52f84a2b3e4323a9875dcbfe9d24952941b608
tetema <tetema@student.wethinkcode.co.za>
2022-03-11 06:58:43 +0200

    First project commit

---
+++ dms/src/main/java/za/co/wethinkcode/dms/DeveloperManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package za.co.wethinkcode.dms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeveloperManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeveloperManagementSystemApplication.class, args);
+	}
+
+}
---
+++ dms/src/test/java/za/co/wethinkcode/dms/DeveloperManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.wethinkcode.dms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeveloperManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
