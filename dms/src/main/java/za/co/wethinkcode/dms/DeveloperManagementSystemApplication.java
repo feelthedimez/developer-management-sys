a753ab0158b7a92365b3bfd292b85c5207838e0f
tetema <tetema@student.wethinkcode.co.za>
2022-03-15 09:30:06 +0200

    Revert "started the class"
    This reverts commit e6a6a38dcfbe80b53c8ea358aaf0f0d9ab740381.

--- dms/src/main/java/za/co/wethinkcode/dms/DeveloperManagementSystemApplication.java
+++ dms/src/main/java/za/co/wethinkcode/dms/DeveloperManagementSystemApplication.java
@@ -3,10 +3,6 @@ package za.co.wethinkcode.dms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-
 @SpringBootApplication
 public class DeveloperManagementSystemApplication {
 
--- dms/src/main/java/za/co/wethinkcode/dms/absenteeismFeature/dtos/Availability.java
+++
@@ -1,7 +0,0 @@
-package za.co.wethinkcode.dms.absenteeismFeature.dtos;
-
-public class Availability {
-    private Checks checkInAndCheckOut;
-
-    private String username;
-}
